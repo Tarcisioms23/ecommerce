576db7152757a1b25d68e1fe28fc906a128ff96f
Tarcisio Moreira <tarcisioms23@hotmail.com>
2019-10-18 10:07:03 -0300

    Validacao customizada: email nao repetido na atualização do cliente

--- src/main/java/com/tarcisio/moreira/dto/ClienteDTO.java
+++ src/main/java/com/tarcisio/moreira/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.tarcisio.moreira.domain.Cliente;
+import com.tarcisio.moreira.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
