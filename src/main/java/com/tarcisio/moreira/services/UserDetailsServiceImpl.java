945792353994b0a8b2d56db77e605f49197c0ce7
Tarcisio Moreira <tarcisioms23@hotmail.com>
2019-10-29 17:34:46 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/tarcisio/moreira/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.tarcisio.moreira.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.tarcisio.moreira.domain.Cliente;
+import com.tarcisio.moreira.repositories.ClienteRepository;
+import com.tarcisio.moreira.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+	
+}
