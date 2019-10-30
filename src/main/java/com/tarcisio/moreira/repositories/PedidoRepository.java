6f2c2daeadb02153d86713bd7edebbfbe7484864
Tarcisio Moreira <tarcisioms23@hotmail.com>
2019-10-30 15:11:32 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/tarcisio/moreira/repositories/PedidoRepository.java
+++ src/main/java/com/tarcisio/moreira/repositories/PedidoRepository.java
@@ -1,13 +1,17 @@
 package com.tarcisio.moreira.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+import com.tarcisio.moreira.domain.Cliente;
 import com.tarcisio.moreira.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>  {
 	
-	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
